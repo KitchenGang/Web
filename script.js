0dd1da1e857a9a9f391e919a25b2f2834d9fe4a3
Kitchen <andrewstern663@gmail.com>
2023-07-01 10:28:08 +0300

    v4

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 
 let d = new Date();
 document.body.innerHTML = "<h2 class='time'>" + d + "</h2>"
+
+@keyframes dynamicText {
+    0% {
+      color: #ffffff;
+    }
+    50% {
+      color: #8bc34a;
+    }
+    100% {
+      color: #ffffff;
+    }
+  }
